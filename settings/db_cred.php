b787e905ce2d3cbd77f56dd21d5166edd2afdaf0
BigDramani1 <70725785+BigDramani1@users.noreply.github.com>
2022-10-22 13:23:38 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,7 +1,11 @@
+
 <?php
-//Database credentials
-define('SERVER', 'localhost');
-define('HOSTNAME','us-cdbr-east-06.cleardb.net');
-define('PASSWORD', '791c7e0d');
-define('DATABASE', 'heroku_4351a7d9a2694af');
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
 ?>
