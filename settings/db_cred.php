c59faa2ab6b841b86188ef97855e006d58a2c8c9
BigDramani1 <70725785+BigDramani1@users.noreply.github.com>
2022-10-22 12:41:49 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,7 +1,7 @@
 <?php
 //Database credentials
 define('SERVER', 'localhost');
-define('HOSTNAME','root');
-define('PASSWORD', '');
-define('DATABASE', 'shoppn');
+define('HOSTNAME','us-cdbr-east-06.cleardb.net');
+define('PASSWORD', '791c7e0d');
+define('DATABASE', 'heroku_4351a7d9a2694af');
 ?>
