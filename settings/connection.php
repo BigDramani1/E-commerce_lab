c82afd87175dbeedec165344f9154bf9d63534c6
BigDramani1 <70725785+BigDramani1@users.noreply.github.com>
2022-10-24 09:48:53 +0000

    Update connection.php

--- settings/connection.php
+++ settings/connection.php
@@ -14,7 +14,7 @@ class Connection{
 	function connection(){
 
 		// connect to the database
-		$this->db = mysqli_connect(user, host, password, database);
+		$this->db = mysqli_connect(SERVER, USERNAME, PASSWORD, DATABASE);
 
 		// if there are errors, return false
 		if(mysqli_connect_errno()){
